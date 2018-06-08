964f0803819837282e21fd481f5f74b02c73262c
Tomasz Kiełbowicz <t.kielbowicz@gmail.com>
2018-06-08 20:28:06 +0200

    End program on press of enter key

--- src/main/java/eu/gobio/jape/example/Main.java
+++ src/main/java/eu/gobio/jape/example/Main.java
@@ -8,7 +8,7 @@ import java.net.URI;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, ClassNotFoundException, IOException {
+    public static void main(String[] args)  {
         Jape.install();
         startBrowser();
         AsyncComputation asyncComputation = new AsyncComputation();
@@ -20,13 +20,16 @@ public class Main {
     }
 
     private static void startBrowser() {
-        CompletableFuture.runAsync(()-> {
+        CompletableFuture.runAsync(() -> {
             try {
                 System.out.println("Waiting for server...");
                 Thread.sleep(3000);
-                System.out.println("Starting browser (http://localhost:5005/)");
+                System.out.println("Opening url http://localhost:5005/ ...");
                 Desktop.getDesktop().browse(URI.create("http://localhost:5005/"));
-            } catch (Exception e){
+                System.out.println("\n\nPress >> ENTER << to exit\n\n");
+                System.in.read();
+                System.exit(0);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
